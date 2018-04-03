235e5f4261bfd79acdff94cec900651b3823430d
Kathleen Oneal <onealkathleena@johndeere.com>
2018-04-03 17:08:00 -0500

    more honor code

--- src/main/java/HonorRole/HonorRole.java
+++ src/main/java/HonorRole/HonorRole.java
@@ -5,22 +5,58 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HonorRole {
     private DataSource data;
 
     public HonorRole(DataSource data){
-
         this.data = data;
     }
 
     public List<String> getHonorRoleStudents() {
+        Double minGPA = 2D;
+        return getStudentsByGPA(minGPA);
+    }
+
+    public List<String> getCumLaudeStudents() {
+        Double minGPA = 3.5;
+        return getStudentsByGPA(minGPA);
+    }
+
+    private List<String> getStudentsByGPA(Double minGPA) {
         List<Student> students = data.getStudents();
         List<String> studentNames = new ArrayList<>();
         for(Student student: students){
-            if(student.getGPA()>2)
+            if(student.getGPA()>=minGPA)
                 studentNames.add(student.getName());
         }
+        return studentNames;
+    }
+
+    public List<String> getSummaCumLaudeStudents() {
+        List<Student> students = data.getStudents();
+        List<String> studentNames = new ArrayList<>();
+
+        Student topGPA = new Student();
+        topGPA.setName("jack");
+        topGPA.setGPA(0.0);
+        Student secondGPA = new Student();
+        secondGPA.setGPA(0.0);
+        secondGPA.setName("Mill");
+        for (Student student: students){
+            if (secondGPA.getGPA() > topGPA.getGPA()){
+                topGPA = secondGPA;
+            }
+            if (student.getGPA() > topGPA.getGPA()){
+                topGPA = student;
+            }else if (student.getGPA() > secondGPA.getGPA()){
+                secondGPA = student;
+            }
+
+        }
+        studentNames.add(topGPA.getName());
+        studentNames.add(secondGPA.getName());
         return studentNames;
     }
 }
--- src/test/java/HonorRole/HonorRoleTest.java
+++ src/test/java/HonorRole/HonorRoleTest.java
@@ -1,5 +1,6 @@
 package HonorRole;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,29 +12,57 @@ import static org.mockito.Mockito.when;
 
 public class HonorRoleTest {
 
-    @Test
-    public void whenGetHonorRoleStudentsthenGPAGreaterThan2(){
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private Student student1 = new Student();
+    private Student student2 = new Student();
+    private Student student3 = new Student();
+    private Student student4 = new Student();
+    private DataSource dataSourceMock;
+    private HonorRole honorRole;
 
-        DataSource data = mock (DataSource.class);
-
-        ArrayList<Student> student = new ArrayList<Student>();
-
-        Student student1 = new Student();
-        Student student2 = new Student();
+    @Before
+    public void Before(){
         student2.setName("Jackson");
         student2.setGPA(1.9);
         student1.setName("Kyle");
         student1.setGPA(4.0);
-        student.add(student1);
-        student.add(student2);
-        when(data.getStudents()).thenReturn(student);
+        student3.setName("Larry");
+        student3.setGPA(1.0);
+        student4.setName("Karen");
+        student4.setGPA(3.5);
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        dataSourceMock = mock(DataSource.class);
+        honorRole = new HonorRole(dataSourceMock);
+    }
 
-        HonorRole HR = new HonorRole(data);
-        List<String> names = HR.getHonorRoleStudents();
+    @Test
+    public void whenGetHonorRoleStudentsthenGPAGreaterThan2(){
+        when(dataSourceMock.getStudents()).thenReturn(students);
+        List<String> names = honorRole.getHonorRoleStudents();
+        assertEquals(2,names.size());
+        assertTrue(names.contains("Kyle"));
+        assertTrue(names.contains("Karen"));
+    }
 
-        assertEquals(1,names.size());
-        assertEquals("Kyle",names.get(0));
+    @Test
+    public void whenGetCumLaudeStudentsthenGPAGreaterthan35(){
+        when(dataSourceMock.getStudents()).thenReturn(students);
+        List<String> names = honorRole.getCumLaudeStudents();
+        assertEquals(2, names.size());
+        assertTrue(names.contains("Kyle"));
+        assertTrue(names.contains("Karen"));
+    }
 
+    @Test
+    public void whenGetSummaCumLaudeStudentsthenTopTwoGPA(){
+        when(dataSourceMock.getStudents()).thenReturn(students);
+        List<String> names = honorRole.getSummaCumLaudeStudents();
+        assertEquals(2, names.size());
+        assertTrue(names.contains("Kyle"));
+        assertTrue(names.contains("Karen"));
     }
 
 
